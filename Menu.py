f80a18fc2758d3b2995fe9449730eced17562465
javierhannajulia <hannajuliajavier@gmail.com>
2024-11-25 17:20:59 +0800

    menu updated

--- Menu.py
+++ Menu.py
@@ -81,6 +81,31 @@ def showLibrary():
             s+=f"\nTitle: {items[0]}\nArtist: {items[1]}\nAlbum: {items[2]}\nDuration: {items[3]}\n"
     s+="<----End of Library----->"
     print(s)
+
+def createPlaylist():
+    """Create a new playlist."""
+    name = input("Enter Playlist Name: ")
+    with open("Playlists.csv", "a", newline='') as writer:
+        write = csv.writer(writer)
+        write.writerow([name])
+    print(f"Playlist '{name}' created successfully.")
+
+def showPlaylists():
+    """Display all playlists."""
+    print("<----- Playlists ----->")
+    try:
+        with open("Playlists.csv", mode='r', newline='') as reader:
+            read = csv.reader(reader)
+            for i, items in enumerate(read, 1):
+                print(f"[{i}] {items[0]}")
+    except FileNotFoundError:
+        print("No playlists available. Create one first.")
+    print("<---- End of Playlists ----->")
+
+def addToPlaylist():
+    pass
+# pahelp ko diri nga part huhu
+
 line1 = "<---Welcome to Python Music Player--->"
 
 #Start
@@ -96,69 +121,84 @@ if __name__ == "__main__":
             print("Invalid Input. Please Enter a number.")
             continue
 
-        if first == 1: # Music Library
-            while True:
-                printmenu(menu1)
-                break
+        if first == 1: # View Queue
+            queue_list = list(queue.queue)
+            if queue_list:
+                print("<----- Current Queue ----->")
+                for i, track in enumerate(queue_list, 1):
+                    print(f"[{i}] {track}")
+                print("<----- End of Queue ----->")
+            else:
+                print("Queue is empty.")
 
         elif first == 2: # View Playlists
             # with open('Playlist.csv', mode='r',newline='') as reader:
             #     read=csv.reader(reader)
             # manager=open('Playlists.csv')
             # writer=csv.writer(manager)
+            showPlaylists()
             while True:
-                with open('Playlists.csv', mode='r',newline='') as reader:
-                    read=csv.reader(reader)
-                    count=0
-                    for i in read:
-                        count+=1
-                    if count == 0:
-                        print("You have no Playlists!\n[1] Create New Playlist\n[0] Return")
-                        two=input("Enter Choice: ")
-                        if two == "1":
-                            name=input("Enter Playlist Name: ")
-                            choose=input("\nAdd Songs to your Playlist\n[1] Choose from Library\n[2] Add Custom Track\n[0] Return")
-                            if choose=='1':#choose from library
-                                pass
-                            elif choose=='2': #add custom
-                                pass
+                choice = input("\n[1] Create New Playlist\n[2] Add Track to Playlist\n[0] Return\nEnter Choice: ")
+                if choice == "1":
+                    createPlaylist()
+                elif choice == "2":
+                    addToPlaylist()
+                elif choice == "0":
+                    break
+                else:
+                    print("Invalid choice. Try again.")
 
-                            elif choose=='0':
-                                break
-                            else:
-                                continue
+            # while True:
+            #     with open('Playlists.csv', mode='r',newline='') as reader:
+            #         read=csv.reader(reader)
+            #         count=0
+            #         for i in read:
+            #             count+=1
+            #         if count == 0:
+            #             print("You have no Playlists!\n[1] Create New Playlist\n[0] Return")
+            #             two=input("Enter Choice: ")
+            #             if two == "1":
+            #                 name=input("Enter Playlist Name: ")
+            #                 choose=input("\nAdd Songs to your Playlist\n[1] Choose from Library\n[2] Add Custom Track\n[0] Return")
+            #                 if choose=='1':#choose from library
+            #                     pass
+            #                 elif choose=='2': #add custom
+            #                     pass
+
+            #                 elif choose=='0':
+            #                     break
+            #                 else:
+            #                     continue
                 
-                        elif two=='0':
-                            break
-                        else:
-                            print("Invalid Choice. Try Again!")
-                            continue
-                    elif count !=0:
-                        print("naa")
-                        break
+            #             elif two=='0':
+            #                 break
+            #             else:
+            #                 print("Invalid Choice. Try Again!")
+            #                 continue
+            #         elif count !=0:
+            #             print("naa")
+            #             break
                     
         elif first == 3: # Add Tracks
             while True:
                 printmenu(add)
-                three=int(input("Enter Choice: "))
+                try:    
+                    three=int(input("Enter Choice: "))
+                except ValueError:
+                    print("Invalid input.")
+                    continue
                 if three==1:
                     addtoLibrary()
-                    
                 elif three == 2:
-                    pass
+                    addToPlaylist()
                 elif three==0:
                     break
-                else:
-                    continue
 
-        elif first == 4: # Add Playlist
-            while True:
+        elif first == 4: # Music Library
                 showLibrary()
-                break
             
-
         elif first==0: # Quit
             break
 
         else:
-            continue
+            print("Invalid option. Try again.")
